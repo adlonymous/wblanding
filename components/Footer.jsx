553b4305d3a305072330109e8db57c4c8f0fcb4b
Aadil Ahmed <adlonymous@gmail.com>
2023-08-13 14:40:56 +0000

    Added Beta link

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -17,9 +17,9 @@ const Footer = () => (
     <div className={`${styles.innerWidth} mx-auto flex flex-col gap-8`}>
       <div className="flex items-center justify-between flex-wrap gap-5">
         <h4 className="font-bold md:text-[64px] text-[44px] text-white">
-          Sign up for early access
+          Access our Public Beta now!
         </h4>
-        <a href='https://pvhlcymwvc7.typeform.com/to/JXoNS7Ow'>
+        <a href='https://app.wannabuidl.com'>
         <button type="button" className="flex items-center h-fit py-4 px-6 bg-[#25618B] rounded-[32px] gap-[12px]">
           <img
             src="/headset.svg"
